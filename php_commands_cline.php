293f0d52e7e709c8592d038ce98a1c4e9b5862aa
eua1024 <41024@i.ua>
2018-09-22 20:16:14 +0300

    Add command line version

---
+++ php_commands_cline.php
@@ -0,0 +1,21 @@
+<?php
+	$list = get_defined_functions();
+
+	$result = [];
+	foreach ($list['internal'] as $item) {
+		if (preg_match_all("/Function \[ <internal:(.*)> function (.*) \] {/", exec("php --rf ".$item." | grep function"), $matches)) {
+			$result[$matches[1][0]][] = $matches[2][0];
+		}
+	}
+
+	echo PHP_EOL."PHP version: ".phpversion().PHP_EOL;
+	echo "Binary file: ".PHP_BINARY.PHP_EOL;
+
+	foreach ($result as $library => $lib_commands) {
+		echo PHP_EOL."= PHP ".$library." library =".PHP_EOL;
+
+		foreach ($lib_commands as $command) {
+			echo $command.PHP_EOL;
+		}
+	}
+?>
